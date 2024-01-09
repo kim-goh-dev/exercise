ee964502026704586a2e1d8491233a00d6bdf8a7
kim-goh-dev <kimhuagoh@gmail.com>
2024-01-09 11:39:42 +0000

    small coding fun

---
+++ CodingForFun/CodingForFun/PrintTriangle.cs
@@ -0,0 +1,28 @@
+﻿namespace CodingForFun
+{
+    public class PrintTriangle
+    {
+        public void PrintTriangleResult()
+        {
+            int star = 7;
+            int length = 4;
+            for (int i = 0; i < length; i++)
+            {
+                var numberOfSpaces = 2 * i;
+                for (int j = 0; j < numberOfSpaces; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                // print *
+                for (int j = 0; j < star; j++)
+                {
+                    Console.Write("* ");
+                }
+
+                star -= 2;
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ CodingForFun/CodingForFun/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CodingForFun
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrintTriangle fun = new PrintTriangle();
+            fun.PrintTriangleResult();
+        }
+    }
+}
